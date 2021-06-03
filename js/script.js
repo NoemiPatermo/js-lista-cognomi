e92696f4133c9146d7c84019bf9b34e2e71e685c
noemi86-cpu <noemipatermo@gmail.com>
2021-06-03 16:18:25 +0200

    esercizio

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@
 //3. stampa la lista ordinata alfabeticamente
 //4. scrivi anche la posizione “umana” (partendo da 1) 
 //della lista in cui il nuovo utente si trova
+var cognomeUtente = prompt('prego, inserire cognome');
+var listaCognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+listaCognomi.push(cognomeUtente);//ho inserito il mio cognome
+listaCognomi.sort();//ho riordinato l'array col mio cognome
+//console.log(listaCognomi);
+for (var i = 1; i < listaCognomi.length; i++){
+    
+}
