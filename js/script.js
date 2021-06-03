4454a1b93be99600143b606ce9c4521a90cfaf72
noemi86-cpu <noemipatermo@gmail.com>
2021-06-03 14:20:32 +0200

    esercizio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//1. chiedi all’utente il cognome
+//2. inseriscilo in un array dove sono presenti già altri cognomi: 
+//‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+//3. stampa la lista ordinata alfabeticamente
+//4. scrivi anche la posizione “umana” (partendo da 1) 
+//della lista in cui il nuovo utente si trova
