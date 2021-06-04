12fdd2a1a6ab1b0fdaa1699880cf3fc30774d731
noemi86-cpu <noemipatermo@gmail.com>
2021-06-04 14:43:39 +0200

    esercizio

--- js/script.js
+++ js/script.js
@@ -6,16 +6,27 @@
 //della lista in cui il nuovo utente si trova
 var cognomeUtente = prompt('prego, inserire cognome');
 var listaCognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
-listaCognomi.push(cognomeUtente);//ho inserito il mio cognome
-listaCognomi.sort();//ho riordinato l'array 
-//console.log(listaCognomi);
 
-//ho stampato la mia lista cognomi ordinata 
-//alfabeticamente
+//devo fare in modo che se l'utente inserisce il cognome in minuscolo, questo venga automaticamente messo in maiuscolo
+
+cognomeUtente =  cognomeUtente[0].toUpperCase() + cognomeUtente.slice(1);
+//prendi la stringa cge parte dalla posizione 1 
+
+listaCognomi.push(cognomeUtente);//ho inserito il mio cognome
+
+console.log(listaCognomi);
+
+listaCognomi.sort();//ho riordinato l'array 
+
+console.log(listaCognomi);
+
+var position = listaCognomi.indexOf(cognomeUtente) +1;
+
+/*ho stampato la mia lista cognomi ordinata 
+alfabeticamente
 for (var i = 0; i < listaCognomi.length; i++){
     document.getElementById('position').innerHTML = listaCognomi + '\n' + ' il posto del mio  cognome in ordine alfabetico è  ' +  parseInt(listaCognomi.indexOf(cognomeUtente) + 1) ;
 }
-
-
 //ho trovato la posizione che il mio cognome ha nell'array, con l'idex of
 
+*/
